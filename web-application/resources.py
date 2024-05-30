3f4cc6b158a430f8d544b59d7424b6f79179880b
thaksin5 <128176822+GaoWeiChang@users.noreply.github.com>
2024-05-30 11:02:52 +0800

    edit text

--- web-application/resources.py
+++ web-application/resources.py
@@ -9,6 +9,6 @@ def resources_page():
     st.markdown("""
     - [My Notebook](https://www.kaggle.com/code/charanchang/text-summarization)
     - [Project Repository](https://github.com/GaoWeiChang/Text-Summarization)
-    - [Dataset](https://www.kaggle.com/datasets/nileshmalode1/samsum-dataset-text-summarization)
+    - [Datasets](https://www.kaggle.com/datasets/nileshmalode1/samsum-dataset-text-summarization)
     """)
     
